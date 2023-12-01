ed92e70594a24fe5067a719b276e3746f4aec670
Wim Ulkeman <wimulkeman@hotmail.com>
2023-12-01 16:48:54 +0100

    chore(exercise): add exercise with collection
    Adding an exercise to test:
    - against boolean
    - against instance of
    - assert array contains value

---
+++ src/Handler/PageHandler.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Handler;
+
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+
+class PageHandler
+{
+    public function supports(string $type)
+    {
+        return 'page' === $type;
+    }
+
+    public function handle(string $value): Collection
+    {
+        $collection = new ArrayCollection([
+            uniqid('foo-',true),
+            'foo',
+            uniqid('bar-', true),
+            $value,
+        ]);
+
+        return $collection;
+    }
+}
